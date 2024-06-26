4b9e130e45f8d7fd2be7d77e6246bfafd6cf2e51
Jglauby <jglauby@gmail.com>
2024-06-26 15:15:52 -0500

    Add project files.

---
+++ OpJosMod-SlapshotRebound/Patches/PlayerControllerPatch.cs
@@ -0,0 +1,35 @@
+﻿using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP.UnityEngine;
+using HarmonyLib;
+using Michsky.UI.ModernUIPack;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace OpJosModSlapshotRebound.TestMod.Patches
+{
+    [HarmonyPatch(typeof(PlayerController))]
+    internal class PlayerControllerPatch
+    {
+        private static ManualLogSource mls;
+        public static void SetLogSource(ManualLogSource source)
+        {
+            mls = source;
+        }
+
+        private static bool alreadyPressed = false;
+        private static KeyCode key = KeyCode.P;
+
+        [HarmonyPatch("Update")]
+        [HarmonyPostfix]
+        private static void UpdatePatch(PlayerController __instance)
+        {
+            if (Input.GetKeyInt(key) && alreadyPressed == false)
+            {
+                mls.LogInfo("P pressed");
+            }
+
+            alreadyPressed = Input.GetKeyInt(key);
+        }
+    }
+}
---
+++ OpJosMod-SlapshotRebound/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using BepInEx.Unity.IL2CPP;
+using HarmonyLib;
+using OpJosModSlapshotRebound.TestMod.Patches;
+
+namespace OpJosModSlapshotRebound.TestMod
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class OpJosModSlapshotRebound : BasePlugin
+    {
+        private const string modGUID = "OpJosModSlapshotRebound.TestMod";
+        private const string modName = "TestMod";
+        private const string modVersion = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static OpJosModSlapshotRebound Instance;
+
+        internal ManualLogSource mls;
+
+        //not needed?
+        public override void Load()
+        {
+            Loaded();
+        }
+
+        void Awake()
+        {
+            Loaded();
+        }
+
+        private void Loaded()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+
+            mls.LogInfo(modName + " has loaded");
+
+            PlayerControllerPatch.SetLogSource(mls);
+            harmony.PatchAll();
+        }
+    }
+}
